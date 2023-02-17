67662af0cd83ad5ea735e998453fdf9186b9f35a
cvg2 <123977040+cvg2@users.noreply.github.com>
2023-02-17 11:41:44 -0500

    Carmen Update App.js
    Added the new balance entries, and prepare the transactions for RTDB and local state

--- src/App.js
+++ src/App.js
@@ -38,3 +38,49 @@ class App extends Component {
       console.log(`[ERROR]: INVALID ENTRY`)
       return null;
     }
+    
+   
+    //Prepare new balance entries
+    const newBalance = this.state.balance + +(txn.amount);
+    const newIncome = this.state.income + (+(txn.amount) > 0 ? +(txn.amount) : 0);
+    const newExpense = this.state.expense + (+(txn.amount) < 0 ? +(txn.amount) : 0);
+
+    //Send new balance entries to RTDB
+    this.db.updateBalance(newBalance, newIncome, newExpense);
+
+    //Send new transaction to RTDB
+    const txnId = this.db.updateTxns(txn.text, txn.amount); //Get transaction ID
+
+    //Prepare new transaction entry for local state
+    const newTxns = this.state.transactions;
+    newTxns[txnId] = {
+      amount: +(txn.amount),
+      text: txn.text
+    };
+
+    //Set local state to new entries (NOTE: will cause rerender of page)
+    this.setState({
+      balance: newBalance,
+      income: newIncome,
+      expense: newExpense,
+      transactions: newTxns
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Expense Tracker by Group 6</h1>
+        <Balanace 
+          balance={this.state.balance} 
+          income={this.state.income} 
+          expense={this.state.expense}
+        />
+        <History transactions={this.state.transactions}/>
+        <UserInput app={this} addTxn={this.addTxn}/>
+      </div>
+    );
+  }
+}
+
+export default App;
