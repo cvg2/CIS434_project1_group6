e1f3d70015d2bb0cce751bd3ee149b31cda0baa8
KamMubarak <116052450+KamMubarak@users.noreply.github.com>
2023-02-17 12:04:52 -0500

    Update firebasedb.js
    adding returnUserId function

--- src/firebasedb.js
+++ src/firebasedb.js
@@ -40,6 +40,63 @@ var userId = null;
 //Class used to interact with user's data in the Firebase Realtime Database (RTDB)
 class Database {
 
+  //Retrieves user ID from cookie. If no cookie present, generate new user ID that does
+  //not exist in RTDB. Create new entry for newly created user ID with default parameters.
+  async returnUserId() {
+
+    //User reference to check if created user ID already present in RTDB
+    const dbRef = ref(db);
+    const users = (await get(child(dbRef, `users`))).val();
+
+    //Retreive user's cookies in order for ID fetch
+    const cookieList = decodeURIComponent(document.cookie).split("; ");
+    
+    //Assign 'uID' to 'userId' from cookie, if present. If not present, leave as null
+    var uID = null;
+    cookieList.forEach((cookie) => {
+      if (cookie.indexOf("userId") == 0) {
+          uID = cookie.substring(7);
+      }
+    });
+  
+    //Set unique 'userId' via cookie if no 'userId' cookie is present
+    if (uID == null) {
+      console.log(`[DATABASE]: NO USER ID FOUND, CREATING NEW USER ID`);
+  
+      //Generate a unique user ID not already present in RTDB
+      do {
+        uID = Math.floor(Math.random() * 10000);
+      } while(uID in users);
+  
+      //Generate cookie expiration date
+      const exdays = new Date();
+      exdays.setTime(exdays.getTime() + 1000 * 60 * 60 * 24 * 365); //Cookie expiration time is a year
+      
+      //Create cookie with set user ID and expiration date
+      document.cookie = `userId=${uID}; expires=${exdays.toUTCString()};`;
+      console.log(`[DATABASE]: ASSIGNED ID '${uID}'`);
+    }
+  
+    //If user ID is not in the RTDB, create new entry with default parameters
+    if (!(uID in users)) {
+      console.log(`[DATABASE]: USER '${uID}' NOT FOUND, CREATING NEW ENTRY`);
+  
+      set(ref(db, `users/${uID}`), {
+        balance: 0,
+        expense: 0,
+        income: 0,
+        transactions: {} //Will not show up in RTDB, assume 'transactions' is empty
+      });
+    }
+
+    //If user ID is present in RTDB, log to terminal
+    else {
+      console.log(`[DATABASE]: USER '${uID}' FOUND`);
+    }
+  
+    userId = uID; //Assign user ID to global 'userId' for other Database methods
+  }
+  
   //Return the data of a specific user
   async getData() {
     const dbRef = ref(db);
