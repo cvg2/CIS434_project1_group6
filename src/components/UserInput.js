8778f7988966f7521f7fc27bba69749217834d8a
cvg2 <123977040+cvg2@users.noreply.github.com>
2023-02-19 16:29:35 -0500

    Update UserInput.js
    Update UserInput with css format

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -14,27 +14,22 @@ class UserInput extends Component {
 
     render() {
         return (
-            <div className="UserInput">
-                <h1>UserInput</h1>
-                <div className="UserInputCont">
-                    <h2>Text</h2>
-                    <input 
-                        id="text" 
-                        name="text"
-                        placeholder="Enter Text"
-                        onChange={this.onChange}
-                    />
-                    <h2>Amount</h2>
-                    <input 
-                        id="amount" 
-                        name="amount"
-                        placeholder="Enter Amount"
-                        onChange={this.onChange}
-                    />
-                </div>
-                <button onClick={this.props.addTxn.bind(this.props.app, this.state)}>
-                    ADD TRANSACTION
-                </button>
+            <div>
+                <form className="card card-body col-md-8 mt-4" onSubmit={this.props.addTxn.bind(this.props.app, this.state)}>
+                    <h4>Add new transaction
+                        <hr></hr>
+                    </h4>
+                    <h5>Text</h5>
+                    <div className="form-group">
+                        <input className="form-control" type="text" name="text" placeholder="Description" 
+                            onChange={this.onChange} value = {this.state.text}/>
+                    </div>
+                    <h5 className="mt-3">Amount</h5>
+                    <h5>(negative - expense, positive - income)</h5>
+                    <input className="form-control" type="number" name="amount" placeholder="Amount"
+                        onChange={this.onChange} value = {this.state.amount}/>
+                    <input className="btn btn-primary btn-block btn-lg mt-3" type="submit" value="Add transaction" />
+                </form>
             </div>
         );
     }
