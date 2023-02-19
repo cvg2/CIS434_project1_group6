cf7dd00ed987cf4ecb579d5b409b9ed5925605fc
cvg2 <123977040+cvg2@users.noreply.github.com>
2023-02-19 16:27:18 -0500

    Update Balance.js
    Carmen update balance.js with css format

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,14 +3,31 @@ import React, { Component } from "react";
 class Balance extends Component {
     render() {
         return (
-            <div className="Balance">
-                <h1>BALANCE</h1>
-                <p> 
-                    Balance: {this.props.balance} <br/>
-                    Income: {this.props.income} <br/>
-                    Expense: {this.props.expense}
-                </p>
-            </div>
+            <div>
+                <div className="card card-body col-md-8 mt-4">
+                    <h4>YOUR BALANCE</h4>
+                    <div>
+                        <h3><strong>{this.props.balance.toLocaleString('en-US', {style: 'currency', currency: 'USD',
+                                    minimumFractionDigits: 2, maximumFractionDigits: 2 })}</strong></h3>
+                        <div className="bg-light p-3 shadow-lg">
+                            <table className="table table-borderless">
+                                <tbody>
+                                    <tr>
+                                        <td><h4>INCOME</h4></td>
+                                        <td><h4>EXPENSE</h4></td>
+                                    </tr>
+                                    <tr>
+                                        <td className="green"><strong><h5>{this.props.income.toLocaleString('en-US', {style: 'currency', currency: 'USD',
+                                            minimumFractionDigits: 2, maximumFractionDigits: 2 })}</h5></strong></td>
+                                        <td className="text-danger"><strong><h5>{this.props.expense.toLocaleString('en-US', {style: 'currency', currency: 'USD',
+                                            minimumFractionDigits: 2, maximumFractionDigits: 2 })}</h5></strong></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>      
         );
     }
 }
