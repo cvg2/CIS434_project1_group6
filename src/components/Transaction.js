0ece2f3a5abb02c4e060b98052380debd991ee76
SalvaMur <121520579+SalvaMur@users.noreply.github.com>
2023-03-04 22:12:23 -0500

    Add delButton BY MICHAEL
    Committed by Salvador, Code by Michael

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 class Transaction extends Component {
     render() {
         const {text, amount} = this.props.data;
+        const tid = this.props.tid;
 
         return (
-            <tr className={`transaction ${this.props.className}`}>
+            <tr id={tid} className={`transaction ${this.props.className}`}>
+                <td className="delButton"><button onClick={this.props.removeTxn.bind(this.props.app, tid)}>&#x2A09;</button></td>
                 <td>{text}</td>
                 <td className="alignRight">{amount.toLocaleString('en-US', {style: 'currency', currency: 'USD',
                             minimumFractionDigits: 2, maximumFractionDigits: 2 })}
